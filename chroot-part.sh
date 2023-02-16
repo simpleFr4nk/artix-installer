4650e1478c443cba4e488ef9555450945151e6ca
SimpleFr4nk <enrico.franco99@gmail.com>
2023-02-16 21:31:01 +0100

    Moved chroot part of the script to another file to make the installation automatic

---
+++ chroot-part.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Set timezone
+ln -sf /usr/share/zoneinfo/$region_city /etc/localtime
+hwclock --systohc
+
+# Localization
+printf "en_US.UTF-8 UTF-8\n" >> /etc/locale.gen
+locale-gen
+
+# Set locale systemwide
+printf "LANG=en_US.UTF-8\nLC_COLLATE="C"\n" > /etc/locale.conf
+printf "KEYMAP=it\n" > /etc/vconsole.conf
+
+# Install boot loader
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Root user
+echo "--Root password--"
+passwd
+
+# Add user
+useradd -m -G wheel $user
+useradd -m -G video $user
+useradd -m -G audio $user
+echo "--$user password--"
+passwd $user
+
+# Set doas privileges
+printf "permit persist :wheel\npermit nopass $user cmd reboot\npermit nopass $user cmd poweroff" >> /etc/doas.conf
+
+# Hostname
+printf "$my_hostname\n" > /etc/hostname
+printf "hostname=\"$my_hostname\"\n" > /etc/conf.d/hostname
+printf "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$my_hostname.localdomain\t$my_hostname\n" > /etc/hosts
+
+# Connman
+rc-update add connmand
--- install.sh
+++ install.sh
@@ -22,9 +22,8 @@ my_hostname="Arti"
 # User
 user="ef"
 
-install_variable () {
-    echo disk=$disk part1=$part1 part2=$part2 part3=$part3 \
-        region_city=$region_city my_hostname=$my_hostname
+chroot_vars () {
+    echo region_city=$region_city my_hostname=$my_hostname user=$user
 }
 
 printf "\nDone with configuration. Installing...\n\n"
@@ -56,44 +55,7 @@ basestrap /mnt linux linux-firmware linux-headers mkinitcpio
 fstabgen -U /mnt > /mnt/etc/fstab
 
 # Chroot
-sudo artix-chroot /mnt 
-
-# Set timezone
-ln -sf /usr/share/zoneinfo/$region_city /etc/localtime
-hwclock --systohc
-
-# Localization
-printf "en_US.UTF-8 UTF-8\n" >> /etc/locale.gen
-locale-gen
-
-# Set locale systemwide
-printf "LANG=en_US.UTF-8\nLC_COLLATE="C"\n" > /etc/locale.conf
-printf "KEYMAP=it\n" > /etc/vconsole.conf
-
-# Install boot loader
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Root user
-printf "--Root password--\n"
-passwd
-# Add user
-useradd -m -G wheel,video,audio $user
-printf "--$user password--\n"
-passwd $user
-
-# Set doas privileges
-printf "permit persist :wheel\npermit nopass $user cmd reboot\npermit nopass $user cmd poweroff" >> /etc/doas.conf
-
-# Hostname
-printf "$my_hostname\n" > /etc/hostname
-printf "hostname=\"$my_hostname\"\n" > /etc/conf.d/hostname
-printf "\n127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$my_hostname.localdomain\t$my_hostname\n" > /etc/hosts
-
-# Connman
-rc-update add connmand
-
-# Exit chroot
-exit
+sudo cp chroot-part.sh /mnt/                    # copy the installer script for the chroot part
+sudo "$(chroot-vars)" artix-chroot /mnt /bin/bash -c 'sh chroot-part.sh ; rm chroot-part.sh ; exit'
 
 printf '\nInstallation finished.\n\nYou need to poweroff and remove the usb.\n'
